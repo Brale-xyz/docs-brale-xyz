153626229bb99c1162dc6e661945702909ab7ba8
kennymiesner <kenny@brale.xyz>
2025-05-05 17:07:33 -0500

    chore: update overview section

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,8 @@
+import MDXComponents from '@theme-original/MDXComponents';
+import { WIPCallout } from '@site/src/components/WIPCallout';
+
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  WIPCallout,
+};
