d1726f11fa43774604629f074f36b558af0b4238
kennymiesner <kenny@brale.xyz>
2025-05-06 15:46:32 -0500

    style: fix intro bottom border

--- src/components/shared/Intro.tsx
+++ src/components/shared/Intro.tsx
@@ -7,10 +7,10 @@ type IntroProps = Readonly<{
 
 export function Intro({ heading, subheading }: IntroProps): ReactNode {
   return (
-    <div className="mb-10 flex justify-between border-b border-gray-300 pb-6 dark:border-gray-700">
+    <div className="mb-10 flex justify-between border-x-0 border-b-[1px] border-t-0 border-solid border-gray-300 pb-6 dark:border-gray-700">
       <div>
         <h1>{heading}</h1>
-        {subheading && <p className="text-xl leading-7">{subheading}</p>}
+        {subheading && <p className="text-xl">{subheading}</p>}
       </div>
     </div>
   );
