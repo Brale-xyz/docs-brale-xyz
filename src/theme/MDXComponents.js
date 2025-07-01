0617a008cfd70d3b86f514f8efe893debb28ee4c
kennymiesner <kenny@brale.xyz>
2025-07-01 12:27:22 -0500

    chore: add inlineimage component, externallink component, and formaturl util

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,9 +1,18 @@
 import MDXComponents from '@theme-original/MDXComponents';
-import { HTTPMethod, Intro, WIPCallout, YouTubeEmbed } from '@site/src/components/shared';
+import {
+  ExternalLink,
+  HTTPMethod,
+  InlineImage,
+  Intro,
+  WIPCallout,
+  YouTubeEmbed,
+} from '@site/src/components';
 
 export default {
   ...MDXComponents,
+  ExternalLink,
   HTTPMethod,
+  InlineImage,
   Intro,
   WIPCallout,
   YouTubeEmbed,
